04a319cd17c0a19cdb9b34f3625f44dcaa09d143
Komal Basit <komalbasit2018@gmail.com>
2024-05-14 09:56:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav__link')
+
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open');
+//     })
+// })
+
+// document.querySelector('.nav-toggle').addEventListener('click', function() {
+//     document.querySelector('.nav').classList.toggle('nav-open');
+//   });
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
